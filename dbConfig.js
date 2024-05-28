c71f8b9284e08ea392f22653b65c2fc772b14971
sxvxn <unormiesable@gmail.com>
2024-05-28 10:05:13 +0700

    login (not finished)

--- dbConfig.js
+++ dbConfig.js
@@ -1,6 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
-const dbFilePath = './public/database/go_sayur.db';
-const connection = new sqlite3.Database(dbFilePath);
+const dbPath = path.join(__dirname, 'public', 'database', 'go_sayur.db');
+const dbConnection = new sqlite3.Database(dbPath, (err) => {
+    if (err) {
+        console.error('Could not connect to database', err);
+    } else {
+        console.log('Connected to database');
+    }
+});
 
-module.exports = connection;
+module.exports = dbConnection;
--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const dbConnection = require('./dbConfig');
+const session = require('express-session');
+
 const app = express();
 const port = 3000;
 
-const dbConnection = require('./dbConfig');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
+
+app.use(session({
+    secret: 'ITS SO SECRET WTF',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get('/session', (req, res) => {
+    if (req.session.user) {
+        res.json({ loggedIn: true, username: req.session.user.nama_user });
+    } else {
+        res.json({ loggedIn: false });
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/pages', 'login.html'));
+});
 
 app.get('/api/products', (req, res) => {
     const query = 'SELECT * FROM produk';
@@ -28,9 +51,44 @@ app.get('/api/product/:id', (req, res) => {
     });
 });
 
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+
+    const query = 'SELECT * FROM user WHERE email_user = ? AND password_user = ?';
+    dbConnection.get(query, [email, password], (error, row) => {
+        if (error) {
+            console.error('Database query error:', error);
+            return res.status(500).send('Internal server error');
+        }
+        if (row) {
+            console.log('Row data:', row);
+
+            req.session.user = { username: row.nama_user };
+            console.log('Session user:', req.session.user);
+            res.redirect('/index.html');
+        } else {
+            res.send('Invalid email or password');
+        }
+    });
+});
+
+
+
+app.get('/logout', (req, res) => {
+    console.log('Attempting to destroy session');
+    req.session.destroy(err => {
+        if (err) {
+            console.error('Error destroying session:', err);
+            res.status(500).send('Internal server error');
+        } else {
+            console.log('Session destroyed successfully');
+            res.redirect('/');
+        }
+    });
+});
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
